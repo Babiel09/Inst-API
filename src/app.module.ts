3ff2ef914ea57d1d481136bb263152f5fbddd2b0
Gabriel Castro <estudosbabiel@gmail.com>
2025-02-05 16:55:44 -0300

    config: Importing the student.module.ts like a import

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from 'prisma/prisma.module';
+import { StudentModule } from './student/student.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, StudentModule],
   controllers: [],
   providers: [],
 })
