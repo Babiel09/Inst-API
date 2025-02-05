59332bc03be397400b3e82da9208146f70003905
Gabriel Castro <estudosbabiel@gmail.com>
2025-02-05 17:16:13 -0300

    feat: Created new Queue

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from 'prisma/prisma.module';
 import { StudentModule } from './student/student.module';
+import { BullModule } from '@nestjs/bull';
+import { STUDENT_QUEUE } from './queues/constants';
 
 @Module({
-  imports: [PrismaModule, StudentModule],
+  imports: [
+    PrismaModule, 
+    StudentModule,
+    BullModule.forRoot({
+      redis:{
+        host:process.env.REDIS_HOST,
+        port:Number(process.env.REDIS_PORT),
+      }
+    }),
+    BullModule.registerQueue({
+      name:STUDENT_QUEUE
+    }),
+  ],
   controllers: [],
   providers: [],
 })
